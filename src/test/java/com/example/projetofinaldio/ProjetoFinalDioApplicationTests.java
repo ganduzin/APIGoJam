cdc7a38b429711157cb627546cb32d1d71dd4eb0
Daniel <dangandour@gmail.com>
2023-10-19 07:29:48 -0300

    first commit

---
+++ src/test/java/com/example/projetofinaldio/ProjetoFinalDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetofinaldio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
